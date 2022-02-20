a2bef37cd27f401024e69f248708e3e9133b2f59
Francois Brill <f@brill.co.za>
2022-02-19 17:51:06 -0800

    Responsiveness for phone demo

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,8 @@ export default function Home() {
         <title>NodCard Example</title>
         <style>:root {`{${primaryColor} ${a11yColor}}`}</style>
       </Head>
-      <div className="min-h-screen grid content-center place-content-center bg-skin-primary bg-opacity-10 bg-edit-dots">
-        <div className="bg-white w-[500px] text-center rounded-2xl px-5 pb-5 shadow-xl">
+      <div className="min-h-screen grid content-center place-content-center bg-skin-primary bg-opacity-10 bg-edit-dots p-5">
+        <div className="bg-white w-full sm:w-[500px] text-center rounded-2xl px-5 pb-5 shadow-xl">
           <div className="w-28 h-28 bg-skin-primary rounded-full mx-auto -mt-14" />
           <div className="m-5">
             <p className="m-2 font-extrabold text-3xl tracking-tight text-skin-primary">
@@ -42,13 +42,13 @@ export default function Home() {
               An optional overview about this person as a quick bio.
             </p>
           </div>
-          <ul className="flex m-5 justify-center space-x-3">
+          <ul className="flex mt-5 sm:my-5 justify-center space-x-3">
             {links.map((link, i) => (
               <li key={i}>
                 <a
                   href={link.href}
                   target="_blank"
-                  className="h-12 grid w-12 place-content-center rounded-lg border border-opacity-40 focus:ring-2 focus:ring-offset-2  bg-skin-primary bg-opacity-30 text-skin-primary hover:bg-opacity-100 hover:text-skin-a11y border-skin-primary focus:ring-skin-primary"
+                  className="h-10 w-10 sm:h-12 sm:w-12 grid place-content-center rounded-lg border border-opacity-40 focus:ring-2 focus:ring-offset-2  bg-skin-primary bg-opacity-30 text-skin-primary hover:bg-opacity-100 hover:text-skin-a11y border-skin-primary focus:ring-skin-primary"
                   rel="noreferrer"
                 >
                   <link.icon className="h-5 w-5" />
@@ -57,7 +57,7 @@ export default function Home() {
             ))}
           </ul>
         </div>
-        <div className="absolute left-[calc(50vw-100px)] w-[200px] bottom-14 rounded-xl bg-white p-5 text-center flex items-center space-x-4 shadow-xl">
+        <div className="absolute left-[calc(50vw-100px)] w-[200px] bottom-5 sm:bottom-14 rounded-xl bg-white p-3 text-center flex items-center space-x-4 shadow-xl">
           <ColorPicker
             color={color}
             onChange={setColor}
